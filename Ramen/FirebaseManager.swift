b7da3ebc37dde238c652d63756e6d4cb69f98ce4
Makoto1997 <mkt1997gg@gmail.com>
2022-05-19 22:06:13 +0900

    FirebaseのセッティングとFirebase用のファイルを追加しました。

---
+++ Ramen/FirebaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseManager.swift
+//  Ramen
+//
+//  Created by Makoto on 2022/05/19.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+final class FirebaseManager {
+    
+    
+}
